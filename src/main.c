566455572b4e01626b93da9884f23fe42753b653
soulaiman <84672766+solaymane403@users.noreply.github.com>
2026-08-18 10:02:46 +0100

    v1

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char input[1024];
+
+    while (1)
+    {
+        printf("mini-shell > ");
+
+        if (fgets(input, sizeof(input), stdin) == NULL)
+            break;
+
+        printf("You typed: %s", input);
+    }
+
+    return (0);
+}
